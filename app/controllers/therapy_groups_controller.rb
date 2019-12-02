52d099d02d1a7ebff11c029b9170bfbc2632c3c8
yasmine-hj <yasminehajaji95@gmail.com>
2019-12-02 16:10:21 +0000

    therapy_groups_controller update

--- app/controllers/therapy_groups_controller.rb
+++ app/controllers/therapy_groups_controller.rb
@@ -1,2 +1,31 @@
 class TherapyGroupsController < ApplicationController
+    def index
+        @therapy_groups = TherapyGroup.all
+    end
+
+    def new
+        @therapy_group = TherapyGroup.new
+        @bourbon.build_therapy_specialty
+    end
+
+    def create
+        @therapy_group = TherapyGroup.new(therapy_group_params)
+        if therapy_group.save
+            redirect_to therapy_group_path
+        else
+            @therapy_group.build_therapy_specialty
+            render :new
+        end
+    end
+
+    def ratings
+        @therapy_groups = TherapyGroup.grouped_ratings
+    end
+
+    private
+
+    def therapy_group_params
+        params.require(:therapy_group).permit(:name, :description, :location, :duration, :therapy_specialty_id, therapy_specialty_attributes: [name])
+    end
+
 end
