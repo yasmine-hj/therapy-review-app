e485e55d70b612afb840da00a6f03bb6fc20238d
yasmine-hj <yasminehajaji95@gmail.com>
2019-12-02 16:15:53 +0000

    therapy_group model update

--- app/models/therapy_group.rb
+++ app/models/therapy_group.rb
@@ -2,4 +2,17 @@ class TherapyGroup < ApplicationRecord
   belongs_to :therapy_specialty
   has_many :reviews
   has_many :users, through: :reviews
+  accepts_nested_attributes_for :therapy_specialty
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :duration, presence: true
+
+  scope :grouped_ratings, -> {left_joins(:reviews).group(:id).order('avg(stars) asc')}
+  
+  def self.alphabetical_order
+    order(:name)
+  end
+
 end
