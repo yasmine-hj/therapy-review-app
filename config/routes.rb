feb4edf371c09807524c8fb4560ea4884baeb7a9
yasmine-hj <yasminehajaji95@gmail.com>
2019-12-02 14:05:19 +0000

    add username name and bio to users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :therapy_groups
   resources :reviews
   devise_for :users
+  
+  
   root to: 'application#welcome'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_095635) do
+ActiveRecord::Schema.define(version: 2019_12_02_135810) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "stars"
+    t.string "content"
+    t.integer "therapy_group_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["therapy_group_id"], name: "index_reviews_on_therapy_group_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "therapy_groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.string "duration"
+    t.integer "therapy_specialty_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["therapy_specialty_id"], name: "index_therapy_groups_on_therapy_specialty_id"
+  end
+
+  create_table "therapy_specialties", force: :cascade do |t|
+    t.string "name"
+    t.string "desciption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +50,14 @@ ActiveRecord::Schema.define(version: 2019_12_02_095635) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "therapy_groups"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "therapy_groups", "therapy_specialties"
 end
