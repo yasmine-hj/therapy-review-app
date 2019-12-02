96ae994c04c9c4fc431f54601ee2e64b6eacb5f9
yasmine-hj <yasminehajaji95@gmail.com>
2019-12-02 15:48:52 +0000

    reviews_controller update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+    def index
+        if @therapy_group = TherapyGroup.find_by_id(params[:therapy_group_id])
+            @reviews = @therapy_group.reviews.all
+        else
+            @reviews = Review.all
+        end
+    end
+
+    def new
+        if @therapy_group = TherapyGroup.find_by_id(params[:therapy_group_id])
+            @reviews = @therapy_group.reviews.build
+        else
+            @reviews = Review.new
+        end
+    end
+
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to review_path(@review)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:stars, :title, :content, :therapy_group_id)
+    end
 end
