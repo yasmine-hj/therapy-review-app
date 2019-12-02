908ad8cbd528457232d22f07b2a94d29f7a81101
yasmine-hj <yasminehajaji95@gmail.com>
2019-12-02 15:36:20 +0000

    registrations controller update

--- app/controllers/registraions_controller.rb
+++ app/controllers/registraions_controller.rb
@@ -2,11 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
 
     private
         def sign_up_params
-            params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+            params.require(:user).permit(:username, :name, :email, :bio, :password, :password_confirmation)
         end
         
         def account_update_params
-            params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+            params.require(:user).permit(:username, :name, :email, :bio, :password, :password_confirmation)
         end
 end
     
