9a824127a62609f317e8946c4402fdcac51bcc26
yasmine-hj <yasminehajaji95@gmail.com>
2019-12-04 09:56:04 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   resources :therapy_specialties
-  resources :therapy_groups
+  resources :therapy_groups do
+    resources :reviews, only: [:new, :index]
+  end
   resources :reviews
   devise_for :users, :controllers => {registrations: 'registrations'}
   root to: 'application#welcome'
